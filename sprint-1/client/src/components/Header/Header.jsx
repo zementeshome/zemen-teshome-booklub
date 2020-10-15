9755b936397dd8b804a240923810bbc7679fa6e1
Zemen Teshome <zementeshome@gmail.com>
2020-10-14 21:15:40 -0400

    header mobile styling

--- sprint-1/client/src/components/Header/Header.jsx
+++ sprint-1/client/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Header.scss';
 
 function Header({handleLogout}) {
     return (
        <section className="header">
            <nav className="header__nav">
+               <img className="header__logo" src={process.env.PUBLIC_URL + '/assets/logo2.svg'} alt="booklub logo" alt=""/>
             <ul className="header__nav-list">
                 <li className="header__nav-list-item">home</li>
                 <li className="header__nav-list-item">about</li>
